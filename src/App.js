2ab7c7b34c556ed6a7be3044a2a3382154ceb40f
Jaiprakash Nagar <jayprakash@softopstechnologies.com>
2024-03-27 14:55:33 +0530

    check the code to push from local to testbranch

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Homes />} />
             <Route path="/demo" element={<Demo />} />
+            <Route path="/Jai"/>
           </Routes>
           <SiteDetails />
           <Footer />
